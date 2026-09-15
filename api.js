6df7f9a5f00c22781720ccd1c0155760698de314
ZPoizon <henriquetobias35@gmail.com>
2026-09-15 12:31:15 -0300

    API Express funcionando

--- api.js
+++ api.js
@@ -1,21 +1,19 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
 app.use(cors());
 
-app.get('/data-hora', (req, res) => {
-    const agora = new Date();
-
+app.get('/', (req, res) => {
     res.json({
-        data: agora.toLocaleDateString('pt-BR'),
-        hora: agora.toLocaleTimeString('pt-BR')
+        date: new Date().toLocaleString('pt-BR'),
+        status: 'API no Render funcionando!'
     });
 });
 
+// Porta dinâmica para o Render
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
-}); 
+});
